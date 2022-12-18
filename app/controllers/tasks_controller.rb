11ac52a55fd7ad6843bc76fcf9e3081164ce8db1
Ajdin Imsirovic <ajdika@live.com>
2022-12-18 13:08:35 +0100

    Try out: rails generate migration DoNothingYet

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,3 +10,32 @@ class TasksController < ApplicationController
   def edit
   end
 end
+
+# A migration is a set of database instructions written in Ruby
+# They "migrate" the db from 1 state to another, essentially, they describe the database changes.
+# Instructions for moving "up" to a new state
+# Instructions for moving "down" to a previous state
+# BENEFITS
+# Keep database schema with application code
+# Executable and repeatable
+# Allows sharing of schema changes
+# Allows writing Ruby instead of SQL
+# Able to access code in the Rails project
+# Use the command line to generate migrations
+  # rails generate migration MigrationName
+# The migration code is a Ruby class with two methods: up and down
+# Change method is a shorthand, contains the same code as the up method would, and Rails knows to do the opposite when it's migrating down
+# Migration helper methods:
+  # create_table
+  # drop_table
+  # rename_table
+  # add_column
+  # remove_column
+  # rename_column
+
+# An alternative way to generate a migration is to generate a model:
+  # rails generate model ModelName columns
+  # for example,
+    # rails generate model Task name:string description:text position:integer completed:boolean
+
+# Try out: rails generate migration DoNothingYet
